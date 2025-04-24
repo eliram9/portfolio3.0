680cbffa67f18ee7f1ffcc9a98c0f11a4201ea8e
eliram9 <eliram9@gmail.com>
2025-04-24 08:41:42 -0400

    fix2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 
 function App() {
     return (
-        <Router>
-            <div className="min-h-screen bg-black">
-                <Navbar />        
-                <Routes>
-                <Route path="/" element={<HomePage />} /></Routes>
-            </div>
-        </Router>
+        <div className="min-h-screen bg-black">
+            <Navbar />        
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </div>
     );
 }
 
